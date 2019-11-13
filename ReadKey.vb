219693de55aaf1557b90954bab7009f0c90f4fe5
EN10 <123>
2019-11-13 13:05:33 +0000

    init

---
+++ ReadKey.vb
@@ -0,0 +1,3 @@
+﻿Module ReadKey
+
+End Module
---
+++ Timer.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Timers
+
+Module Timer
+    Dim blink As Boolean = False
+
+    Sub Main()
+
+        Dim t As New Timers.Timer
+        AddHandler t.Elapsed, AddressOf tick
+        t.Interval = 1000
+        t.Start()
+
+        While True
+        End While
+
+    End Sub
+
+    Private Sub tick(ByVal sender As Object, ByVal e As System.Timers.ElapsedEventArgs)
+
+        If blink Then
+            Console.ForegroundColor = ConsoleColor.Cyan
+            blink = False
+        Else
+            Console.ForegroundColor = ConsoleColor.DarkRed
+            blink = True
+        End If
+
+        Console.Clear()
+        Console.WriteLine("[ ]")
+
+    End Sub
+
+End Module
